bc4955671dc61980ac0fb82d54b203fb7115a4fb
Abhinaw Tiwari <abhinawandyou@gmail.com>
2018-10-20 19:45:25 +0530

    ViewEncapsulation added - default one added, i.e., Emulated. Others are also there like "None and Native"

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
